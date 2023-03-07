6bf1a459a5fd737e97f62fdf9749feefefff2afc
LellaHemasriSai <cs20b020@iittp.ac.in>
2023-03-07 15:25:46 +0530

    created user schema

--- config/dbConnector.js
+++ config/dbConnector.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const dbConnect = () => {
   try {
-    const connect = mongoose.connect("mongodb://localhost:27017/ecommerce");
+    const connect = mongoose.connect(process.env.MONGODB_URL);
     console.log("Database connected successfully!!");
   } catch (error) {
     // throw new Error(error);
