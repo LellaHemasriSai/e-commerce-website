95248ad254c3acce2e1ed14abacf5d57191ce5bc
LellaHemasriSai <cs20b020@iittp.ac.in>
2023-03-16 07:31:20 +0530

    added footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,7 +1,107 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top gap-30">
+                <img src="images/newsletter.png" alt="newletter" />
+                <h2 className=" mb-0 text-white">Sign up for newsletter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-1"
+                  placeholder="Email Address"
+                  aria-label="Email Address"
+                  aria-describedby="basic-addon2"
+                />
+                <span
+                  className="input-group-text p-2 text-white"
+                  id="basic-addon2"
+                >
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">
+                  Indian Institute of Technology, Tirupati <br />
+                  Yerpedu, Tirupati <br />
+                  PinCode: 517619
+                </address>
+                <a
+                  href="tel:+91 1234567890"
+                  className="mt-4 d-block mb-2 text-white"
+                >
+                  +91 1234567890
+                </a>
+                <a
+                  href="mailto:cs20b020@iittp.ac.in"
+                  className="mt-4 d-block mb-2 text-white"
+                >
+                  cs20b020@iittp.ac.in
+                </a>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-middle-links">
+                <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1">Refund Policy</Link>
+                <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1">
+                  Terms and Conditions
+                </Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-middle-links">
+                <Link className="text-white py-2 mb-1">About Us</Link>
+                <Link className="text-white py-2 mb-1">Faq</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-middle-links">
+                <Link className="text-white py-2 mb-1">Laptops</Link>
+                <Link className="text-white py-2 mb-1">Headphones</Link>
+                <Link className="text-white py-2 mb-1">Tablets</Link>
+                <Link className="text-white py-2 mb-1">Watches</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy;{new Date().getFullYear()}; Powered by Hemasri
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
